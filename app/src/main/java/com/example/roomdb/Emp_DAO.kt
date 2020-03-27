44999d61020afc85bcfcd87e62a409c6d9181144
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-03-26 22:43:30 -0600

    Working reading

--- app/src/main/java/com/example/roomdb/Emp_DAO.kt
+++ app/src/main/java/com/example/roomdb/Emp_DAO.kt
@@ -2,11 +2,13 @@ package com.example.roomdb
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface Emp_DAO {
     @Insert
-    fun saveEmp(emp: Emp_Entity){
+    fun saveEmp(emp: Emp_Entity)
 
-    }
+    @Query("select * from Emp_Entity")
+    fun readEmp(): List<Emp_Entity>
 }
--- app/src/main/java/com/example/roomdb/MainActivity.kt
+++ app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.room.Room
 import kotlinx.android.synthetic.*
 
@@ -11,14 +12,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+       // println("->!!!!!!!!!!!!!!!!!!!!!EDUARDO!!!!!!!!!!!!!!!!!!!!!!!!")
+
+
+
         var db= Room.databaseBuilder(applicationContext, AppDB::class.java,"EmployeeDB").build()
 
         Thread{
             var emp = Emp_Entity()
             emp.emp_id = 1
-            emp.emp_name="Eddie"
+            emp.emp_name="Eduardo mejia"
             emp.emp_post="Q&A"
             db.empDAO().saveEmp(emp)
+
+            db.empDAO().readEmp().forEach{
+                Log.i("@EDUARDO", "HELLO!!!!!")
+
+
+
+            }
+
 
         }.start()
 
