1d5fd69dcb97de446f2e791994512efb8817d82d
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-04-09 23:17:37 -0500

    update Login

--- app/src/main/java/com/example/roomdb/MainActivity.kt
+++ app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import androidx.room.Room
 import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mDb:AppDB
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,30 +25,63 @@ class MainActivity : AppCompatActivity() {
 
         var db= Room.databaseBuilder(applicationContext, AppDB::class.java,"EmployeeDB").build()
 
-        Thread{
-            var emp = Emp_Entity()
-            emp.emp_id = 2
-            emp.emp_name ="NILESH"
-            emp.emp_post= "RH"
-          /*
-            emp.emp_id = 3
-            emp.emp_name="Eduardo mejia3"
-            emp.emp_post="Application Developer3"
-            db.empDAO().saveEmp(emp)*/
-            //db.empDAO().delData(emp)
-            db.empDAO().update(emp)
+        // Make the text view scrollable
+        textView.movementMethod = ScrollingMovementMethod()
 
-            db.empDAO().readEmp().forEach{
-                Log.i("@EDUARDO", " ID is ${it.emp_id}")
-                Log.i("@EDUARDO", " Name is ${it.emp_name}")
-                Log.i("@EDUARDO", " Position is ${it.emp_post}")
+        btnUpdate.setOnClickListener {
+            Thread{
+
+                var emp = Emp_Entity()
 
 
+                emp.emp_id = 2
+                emp.emp_name ="NILESH"
+                emp.emp_post= "RRHH4"
+                db.empDAO().update(emp)
+
+
+                /*
+                  emp.emp_id = 3
+                  emp.emp_name="Eduardo mejia3"
+                  emp.emp_post="Application Developer3"
+                  db.empDAO().saveEmp(emp)*/
+                //db.empDAO().delData(emp)
+
+
+                db.empDAO().readEmp().forEach{
+                    Log.i("@EDUARDO", " ID is ${it.emp_id}")
+                    Log.i("@EDUARDO", " Name is ${it.emp_name}")
+                    Log.i("@EDUARDO", " Position is ${it.emp_post}")
+
+                }
+
+            }.start()
+
+        }
+
+        btnSelect.setOnClickListener {
+            //val list = mDb.studentDao().allStudents()
+            doAsync {
+                val list = mDb.empDAO().readEmp()
+
+                uiThread {
+                    toast("${list.size} records found.")
+                    textView.text = ""
+
+                    for (emp in list){
+                        //textView.append("${student.id} : ${student.fullName} : ${student.result}\n")
+                        textView.append("${emp.emp_id}: ${emp.emp_name}: ${emp.emp_post}\n")
+                        println("->!!!!!!!!!!!!!!!!!!!!!EDUARDO!!!!!!!!!!!!!!!!!!!!!!!!")
+
+                    }
+
+                }
 
             }
 
+        }
 
-        }.start()
+
 
 
     }
